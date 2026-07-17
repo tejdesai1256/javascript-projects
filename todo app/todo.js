4dac9fdf8992b7655fbe52c65dc526c6b640ff74
tejdesai1256 <desaitej1256@gmail.com>
2026-07-17 23:47:06 +0530

    added h1

--- todo app/todo.js
+++ todo app/todo.js
@@ -1,8 +1,22 @@
 let btn=document.querySelector('button');
 let ul=document.querySelector('ul');
 let inp=document.querySelector('input');
+let delBtn=document.querySelector('.delete');
 
 btn.addEventListener('click',function(){
-    console.log(inp.value);
+    let item=document.createElement('li');
+    item.innerText=inp.value;
+    let delBtn=document.createElement('button');
+    delBtn.innerText='Delete';
+    delBtn.classList.add('delete');
+    item.appendChild(delBtn);
+    ul.appendChild(item);
     inp.value='';
+});
+
+let delBtns=document.querySelectorAll('.delete');
+delBtns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        e.target.parentElement.remove();
+    });
 });
